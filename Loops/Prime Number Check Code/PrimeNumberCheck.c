8b2054d0400feb663e53bfff1d3c66b338ce9a92
Purno <purnorunjunshqul1@gmail.com>
2026-09-03 15:43:18 +0600

    emni

--- Loops/Prime Number Check Code/PrimeNumberCheck.c
+++ Loops/Prime Number Check Code/PrimeNumberCheck.c
@@ -4,13 +4,13 @@ int main()
 {   int n;
     printf("Enter Number to see if it is Prime or not:");
     scanf("%d", &n);
-    bool ISnotPrime=false;
+    bool ISnotPrime = false;
     for (int i=2;i<=n-1;i++)
     {
         if (n%i==0)
         {
             
-            ISnotPrime=true;
+            ISnotPrime = true;
             break;
         }
        
