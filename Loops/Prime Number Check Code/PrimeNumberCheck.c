d5685d91354cc6b075e6f24b11fa91cdb2f6b2ab
Purno <purnorunjunshqul1@gmail.com>
2026-09-03 15:42:23 +0600

    Primer Number Check

---
+++ Loops/Prime Number Check Code/PrimeNumberCheck.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{   int n;
+    printf("Enter Number to see if it is Prime or not:");
+    scanf("%d", &n);
+    bool ISnotPrime=false;
+    for (int i=2;i<=n-1;i++)
+    {
+        if (n%i==0)
+        {
+            
+            ISnotPrime=true;
+            break;
+        }
+       
+        
+    }
+    if (!ISnotPrime)
+    {
+        printf("%d is a Prime Number",n);
+    }
+    else {
+        printf("%d is a Composite Number",n);
+    }
+   
+}
