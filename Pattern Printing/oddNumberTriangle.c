7094ccc69f266bb3e9938e0d089f6e04e6c36b34
Purno <purnorunjunshqul1@gmail.com>
2026-09-13 19:37:51 +0600

    done

---
+++ Pattern Printing/oddNumberTriangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, i, j, k;
+    printf("Enter Number:");
+    scanf("%d", &a);
+
+    for (i = 1; i <= a; i++)
+    {   k=1;
+        for (j = 1; k <=i; j++)
+        {
+            if (j % 2 != 0)
+            {
+
+                printf("%d", j);
+                k++;
+            }
+        }
+
+        printf("\n");
+    }
+}
