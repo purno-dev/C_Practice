af90ac0c28ec63ffb82bce86494ec83c0f710083
Purno <purnorunjunshqul1@gmail.com>
2026-09-17 01:56:42 +0600

    Right Side Triangle

---
+++ Pattern Printing/TriangleRight.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a,i,j,z;
+    printf("Enter Number of Colummn:");
+    scanf("%d", &a);
+    
+    for (i=1;i<=a;i++)
+    {
+       for (j=1;j<=a-i;j++)
+    {
+        printf("  ");
+    } 
+     for (j=1;j<=i;j++)
+    {
+        printf("* ");
+    } 
+    printf("\n");
+    }
+}
