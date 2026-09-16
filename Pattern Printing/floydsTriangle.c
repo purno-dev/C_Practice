a98eab1910ea5222ec868732cb7286cc8c6c4231
Purno <purnorunjunshqul1@gmail.com>
2026-09-17 01:41:05 +0600

    Floyds Triangle

---
+++ Pattern Printing/floydsTriangle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, a, k=1;
+    printf("Enter Number of Row:");
+    scanf("%d", &a);
+
+    for (i = 1; i <= a; i++)
+    {
+    
+
+        for (int j = 1; j <= i; j++)
+        {
+
+            printf("%d",k);
+            k++;
+        }
+
+        printf("\n");
+    }
+}
