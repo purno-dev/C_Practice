14abb2c1a169394c170906a82c3e886b78469c3d
Purno <purnorunjunshqul1@gmail.com>
2026-09-16 13:43:27 +0600

    Number Nad Alphabet Triangle

---
+++ Pattern Printing/NumberAlphabetTriangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, a, k;
+    printf("Enter Number of Row:");
+    scanf("%d", &a);
+
+    for (i = 1; i <= a; i++)
+    {
+        k = 1;
+        if (i % 2 == 0)
+        {
+            for (int j = 65; k <= i; j++)
+            {
+
+                printf("%c", (char)j);
+                k++;
+            }
+        }
+        else
+        {
+            for (int j = 1; j <= i; j++)
+            {
+
+                printf("%d", j);
+                k++;
+            }
+        }
+        printf("\n");
+    }
+}
