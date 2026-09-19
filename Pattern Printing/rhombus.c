a27a9c181b5bfcb78b9318b709c05b6343a68a9f
Purno <purnorunjunshqul1@gmail.com>
2026-09-19 15:48:14 +0600

    Rhombus

---
+++ Pattern Printing/rhombus.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter Number of Colummn:");
+    scanf("%d", &a);
+    int k=a;
+    for (int i=1;i<=a;i++)
+    {
+           for (int i=1;i<=k;i++)
+    {
+        printf("  ");
+    }
+       for (int i=1;i<=a;i++)
+    {
+        printf("* ");
+    } 
+    printf("\n");
+    k--;
+    }
+}
