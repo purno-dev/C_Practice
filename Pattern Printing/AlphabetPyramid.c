2bda0f120e5ff13ea03dd73a7891c3088ed94f76
Purno <purnorunjunshqul1@gmail.com>
2026-09-19 20:41:49 +0600

    Alphabet Pyramids Done

---
+++ Pattern Printing/AlphabetPyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int a,i,j,z,y;
+    printf("Enter Number of Colummn:");
+    scanf("%d", &a);
+    
+    for (i=1;i<=a;i++)
+    {   z=1;
+        y=65;
+       for (j=1;j<=a-i;j++)
+    {
+        printf("  ");
+    } 
+     for (j=1;z<=i*2-1;j+=2)
+    {
+        
+        printf("%C ",y);
+        y++;
+        z++;
+    } 
+    printf("\n");
+    
+    }
+}
