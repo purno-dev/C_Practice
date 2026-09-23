bd7e55ed6373efe48927db4d4495b53fa2059a44
Purno <purnorunjunshqul1@gmail.com>
2026-09-23 20:01:07 +0600

    Number Pyramid MAST DOne

--- Pattern Printing/t.c
+++ Pattern Printing/t.c
@@ -5,23 +5,15 @@ int main()
     int i, j, k, l, n, a,nsp;
     printf("Enter number of Row:");
     scanf("%d", &n);
-    nsp=n-1;
+    
     for (i = 1;  i <= n; i++)
     {
-        a=i-1;
-         for (k = 1; k<=nsp; k++)
-        {
-            printf("  ");
-            
-        }
-        nsp--;
-        for (j = 1; j<=i; j++)
-        {
-            printf("%d ", j);
-        }
+        
+         a=i-1;
+        
         for (l = 1; l<=i-1; l++)
         {
-            printf("%d ",a);
+            printf("%d",a);
             a--;
         }
 
